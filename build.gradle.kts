52ff288a8968288c69ba850bf4ee4099275bb070
morgan <morgan@dunamu.com>
2021-12-20 13:53:07 +0900

    config for gRPC

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,22 @@
+import com.google.protobuf.gradle.*
+
+buildscript {
+    repositories {
+        maven {
+            setUrl("https://plugins.gradle.org/m2/")
+            mavenCentral()
+        }
+    }
+    dependencies {
+        classpath("com.google.protobuf:protobuf-gradle-plugin:0.8.18")
+    }
+}
+
+
 plugins {
     idea
     kotlin("jvm") version "1.6.10"
+    id("com.google.protobuf") version "0.8.18"
 }
 
 group = "com.sample"
@@ -10,6 +26,32 @@ repositories {
     mavenCentral()
 }
 
+protobuf {
+    protoc {
+        artifact = "com.google.protobuf:protoc:3.19.1"
+    }
+    plugins {
+        id("grpc") {
+            artifact = "io.grpc:protoc-gen-grpc-java:1.42.1"
+        }
+    }
+    generateProtoTasks {
+        ofSourceSet("main").forEach { task ->
+            task.plugins {
+                id("grpc")
+            }
+            task.builtins {
+                create("js")
+            }
+        }
+    }
+}
+
+idea {
+    module {
+        generatedSourceDirs.add(file("${protobuf.protobuf.generatedFilesBaseDir}/main/grpc"))
+    }
+}
 
 tasks {
     test {
@@ -19,6 +61,9 @@ tasks {
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("io.grpc:grpc-netty-shaded:1.43.0")
+    implementation("io.grpc:grpc-protobuf:1.42.1")
+    implementation("io.grpc:grpc-stub:1.43.0")
 
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
 }
